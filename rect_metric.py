d54f56a26bf29ff64031e0aa4c08c822543efa90
msemple1111 <mwsemple@me.com>
2022-03-23 14:23:23 +0000

    new train func

--- rect_metric.py
+++ rect_metric.py
@@ -20,28 +20,25 @@ class RotatedRect:
         return self.get_contour().intersection(other.get_contour())
 
 def compare_grasps(rect1, rect2):
-    cx1 = rect1[0]
-    cy1 = rect1[1]
-    angle1 = rect1[2]
-    w1 = rect1[3]
-    h1 = rect1[4]
-    
-    cx2 = rect2[0]
-    cy2 = rect2[1]
-    angle2 = rect2[2]
-    w2 = rect2[3]
-    h2 = rect2[4]
-    
-    r1 = RotatedRect(cx1, cy1, angle1, w1, h1)
-    r2 = RotatedRect(cx2, cy2, angle2, w2, h2)
+    r1 = RotatedRect(*rect1)
+    r2 = RotatedRect(*rect2)
     
     intersection = r1.intersection(r2).area
     union = (r1.width*r1.height) + (r2.width*r2.height) - intersection
     score = intersection/union
-    
-    print("intersection area: ", intersection)
-    print("union area: ", union)
-    print("score: ", score)
+    return score
+
+def grasp_accuracy(Ys, Y_hats):
+    scores = [compare_grasps(Ys[i], Y_hats[i]) for i, g in enumerate(Ys)]
+    print(scores)
+    avg = lambda x: sum(x)/len(x)
+    return avg(scores)
+
+
+def visul_grasps(rect1, rect2):
+    r1 = RotatedRect(*rect1)
+    r2 = RotatedRect(*rect2)
+    print("score: ", compare_grasps(rect1, rect2))
     
     from matplotlib import pyplot
     from descartes import PolygonPatch
@@ -57,4 +54,4 @@ def compare_grasps(rect1, rect2):
     
     pyplot.show()
 
-#compare_grasps([605.17693, 550.6464, 0, 186.0, 77.1998], [599.86786, 541.52308, 7.896,170.0, 95.7251])
+
